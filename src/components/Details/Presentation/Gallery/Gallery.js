0d7a0b3d21ae5302b5ef76084ebb6142564ea28a
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-06-27 17:41:51 +0200

    Add image gallery in details view

--- src/components/Details/Presentation/Gallery/Gallery.js
+++ src/components/Details/Presentation/Gallery/Gallery.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
+import ImageGallery from 'react-image-gallery';
+
 
 const useStyles = makeStyles({
   root: {
-
+    '& .image-gallery-slide img': {
+      width: '100%',
+      height: '40rem',
+      objectFit: 'cover'
+    }
   }
 })
 
 const Gallery = props => {
+  const { images } = props;
   const classes = useStyles();
-
+  const imagesArray = [];
+  let img = images.split(',').slice(1);
+  img.forEach(element => {
+    imagesArray.push({original: element, thumbnail: element })
+  });
+  console.log(imagesArray);
   return (
-      <section className={classes.root}>Gallery</section>
+    <section className={`${classes.root}`}>
+    
+      <ImageGallery items={imagesArray} />  
+     
+    </section>
   );
 }
 
