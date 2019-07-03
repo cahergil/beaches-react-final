4c8fa08006713f57e0c03410d5192c5ca41d1e23
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-07-03 12:42:42 +0200

    Added translation to description field in details view

--- src/components/Details/Presentation/Description/Description.js
+++ src/components/Details/Presentation/Description/Description.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core';
-import Title from './../../Title/Title';
+import React, { useState} from 'react';
 
-const useStyles = makeStyles({
+import { makeStyles, Button, withStyles } from '@material-ui/core';
+import Title from './../../Title/Title';
+import { green } from '@material-ui/core/colors';
+
+
+const useStyles = makeStyles(theme =>({
   root: {
     gridColumn: '1 / span 2',
     fontSize: '2rem',
     margin: '2rem',
-    
+    [theme.breakpoints.down(800)]: {
+      // gridTemplateColumns: '100%',
+      // gridTemplateRows: 'repeat(3,min-content)',
+      // gridColumnGap: '0rem',
+      // gridRowGap: '2rem'
+      gridColumn: '1 / span 1'
+    } 
   },
   sectionRoot: {
     width: '100%',
@@ -18,18 +27,82 @@ const useStyles = makeStyles({
     boxShadow: '2px 1px 5px #ccc',
     borderRadius: '5px',
     overflow: 'hidden'
+  },
+  description: {
+    margin: '1.5rem',
+    opacity: '0.6'
+  },
+  button: {
+    margin: '1rem'
   }
-})
+}))
+const ColorButton = withStyles(theme => ({
+  root: {
+    color: theme.palette.getContrastText(green[500]),
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  },
+}))(Button);
+
 
 const Description = props => {
-  const { description,colorSchema } = props;
+  const { description, colorSchema } = props;
+  const [translatedDescription, setTranslatedDescription] = useState({text:description ,translated: false})
   const classes = useStyles();
-
+  const handlerTranslate = () => {
+    let fromLang = 'es';
+    let toLang = 'en'
+    let text = translatedDescription.text;
+    const API_KEY = 'AIzaSyCKOeL1RQS5BNIKgPQMuBl8jy30MkuyShA';
+    let url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`;
+    url += '&q=' + encodeURI(text);
+    url += `&source=${fromLang}`;
+    url += `&target=${toLang}`;
+    // console.log(url);
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then((response) => {
+        console.log("response from google: ", response);
+        setTranslatedDescription({
+          text: response.data.translations[0].translatedText,
+          translated: true
+        })
+      })
+      .catch(error => {
+        console.log("There was an error with the translation request: ", error);
+        setTranslatedDescription({
+          text: description,
+          translated: false
+        });
+      });
+    
+  }
   return (
     <section className={classes.root}>
       <div className={classes.sectionRoot}>
         <Title colorSchema={colorSchema} name="Description" />
-      {description}
+        <div>
+          <div className={classes.description}>{translatedDescription.text}</div>
+          <ColorButton
+            variant="outlined"
+            disabled={translatedDescription.translated}
+            onClick={handlerTranslate}
+            color="secondary"
+            className={classes.button}
+          >
+            Translate
+          </ColorButton>
+            
+        </div>
+      
       </div> 
     </section>
   );
--- src/components/Details/Presentation/Presentation.js
+++ src/components/Details/Presentation/Presentation.js
@@ -9,14 +9,20 @@ import Title from '../Title/Title';
 
 // const infoWidth = 35;
 // const galleryWith = 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme =>({
   root: {
     width: '100%',
     display: 'grid',
     // gridTemplateColumns: `${infoWidth}% (100 - ${infoWidth})%`,
     gridTemplateColumns: '34% 64%',
     gridTemplateRows: 'repeat(2, min-content)',
-    gridColumnGap: '2%'
+    gridColumnGap: '2%',
+    [theme.breakpoints.down(800)]: {
+      gridTemplateColumns: '100%',
+      gridTemplateRows: 'repeat(3,min-content)',
+      gridColumnGap: '0rem',
+      gridRowGap: '2rem'
+    }
   },
   sectionRoot: {
     width: '100%',
@@ -28,7 +34,7 @@ const useStyles = makeStyles({
     borderRadius: '5px',
     overflow: 'hidden'
   }
-})
+}))
 
 const Presentation = props => {
   const classes = useStyles();
