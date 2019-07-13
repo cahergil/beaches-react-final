751c83af25fcb39ab7cd3c858e661df3eb1f076b
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-07-13 20:28:35 +0200

    Updates

---
+++ precache-manifest.3db98b76a590969767c4ffa2e90dfe64.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203c25f24f24327d32733ea815271e9e",
+    "url": "/spain-beaches-react/index.html"
+  },
+  {
+    "revision": "7404f24f624740773783",
+    "url": "/spain-beaches-react/static/css/main.03d0fb2a.chunk.css"
+  },
+  {
+    "revision": "ecb3b06e809a75b235b0",
+    "url": "/spain-beaches-react/static/js/2.ceb9994f.chunk.js"
+  },
+  {
+    "revision": "7404f24f624740773783",
+    "url": "/spain-beaches-react/static/js/main.e630205e.chunk.js"
+  },
+  {
+    "revision": "b3ce55a743f38aae8a1f",
+    "url": "/spain-beaches-react/static/js/runtime~main.964795ce.js"
+  },
+  {
+    "revision": "61d013c0652b9e3c58efd9dfedbf20af",
+    "url": "/spain-beaches-react/static/media/beachBannerBlueFlag.61d013c0.jpg"
+  },
+  {
+    "revision": "b53671aadd611c7564f1f962bfe3e6ee",
+    "url": "/spain-beaches-react/static/media/beachBannerNormal.b53671aa.jpg"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/spain-beaches-react/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/spain-beaches-react/static/media/redux.8251fe76.png"
+  }
+]);
