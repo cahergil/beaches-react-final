7b9bf775ed4a081b4220a4a2e358ccb28eb64346
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-07-15 11:51:31 +0200

    Make thumbnails responsive in Gallery

--- src/components/Details/Presentation/Gallery/Gallery.js
+++ src/components/Details/Presentation/Gallery/Gallery.js
@@ -4,29 +4,68 @@ import { makeStyles } from '@material-ui/core';
 import ImageGallery from 'react-image-gallery';
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     '& .image-gallery-slide img': {
       width: '100%',
       height: '40rem',
       objectFit: 'cover'
+    },
+  },
+  thumbnailClassBig: {
+    maxWidth: '10rem',
+    [theme.breakpoints.down(500)]: {
+      display: 'none'
+    },
+    [theme.breakpoints.down(800)]: {
+      maxWidth: '8rem'
     }
-  }
-})
+  },
+  thumbnailClassSmall: {
+    maxWidth: '8rem',
+    [theme.breakpoints.down(500)]: {
+      display: 'none'
+    },
+    [theme.breakpoints.down(800)]: {
+      maxWidth: '5rem'
+    }
+    
+  },
+  thumbnailClassExtraSmall: {
+    maxWidth: '6rem',
+    [theme.breakpoints.up(800)]: {
+      display: 'none'
+    },
+    [theme.breakpoints.down(500)]: {
+      display: 'none'
+    },
+    [theme.breakpoints.down(800)]: {
+      maxWidth: '4rem'
+    }
+  },
+}));
 
 const Gallery = props => {
   const { images } = props;
   const classes = useStyles();
   const imagesArray = [];
   let img = images.split(',').slice(1);
+  let thumbnailClass = classes.thumbnailClassBig;
+  if (img.length >= 10) {
+    thumbnailClass = classes.thumbnailClassExtraSmall;
+  } else if (img.length >= 6) {
+    thumbnailClass = classes.thumbnailClassSmall;
+  }
   img.forEach(element => {
-    imagesArray.push({original: element, thumbnail: element })
+    imagesArray.push({ original: element, thumbnail: element, thumbnailClass: thumbnailClass })
   });
 
   return (
     <section className={`${classes.root}`}>
     
-      <ImageGallery showBullets={true} items={imagesArray} />  
+      <ImageGallery
+        showBullets={true}
+        items={imagesArray} />  
      
     </section>
   );
