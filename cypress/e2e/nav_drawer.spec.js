ae454999ee8c15a2fbe92bdb264c5ca40328d232
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-09-13 19:10:20 +0200

    Complete toolbar and drawer e2e tests

--- cypress/e2e/nav_drawer.spec.js
+++ cypress/e2e/nav_drawer.spec.js
@@ -1,4 +1,6 @@
-describe('toolbar nav and drawer', ()=> {
+/// <reference types="Cypress" />
+
+describe('toolbar nav and drawer nav', ()=> {
 
   it('nav items are present in toolbar in desktop and hidden in drawer', ()=>{
     cy.visit('/spain-map')
@@ -7,7 +9,7 @@ describe('toolbar nav and drawer', ()=> {
     cy.findByText('ABOUT').should('be.visible')
     cy.get('[data-cy=drawer-item]').should('have.length', 0)
   })
- it('nav items are present in drawer in mobile and hidden in desktop', ()=>{
+ it('nav items are present in drawer on mobile and hidden in desktop', ()=>{
     cy.viewport(500, 584)
     cy.visit('/spain-map')
     // burger icon
@@ -20,15 +22,33 @@ describe('toolbar nav and drawer', ()=> {
       cy.contains('SEARCH').should('be.visible')
       cy.contains('ABOUT').should('be.visible')
     })
-    // nav not visible
-    cy.get('[data-cy=nav-item]').within(()=> {
-       cy.contains('SPAIN-MAP').should('not.be.visible')
-       cy.contains('SEARCH').should('not.be.visible')
-       cy.contains('ABOUT').should('not.be.visible')
-      //  cy.findByText('SPAIN-MAP').should('not.be.visible') also works
-     })
-    //  contains('SPAIN-MAP').should('not.be.visible');
+    // toolbar items doesn't exist
+    cy.get('[data-cy=nav-item]').should('not.exist');
   
   })
+
+});
+
+it('drawer closes on item click, mobile', ()=> {
+  cy.viewport(500, 584)
+  cy.visit('/spain-map')
+    // burger icon
+  cy.get('#root > div > header > div > button > span.MuiIconButton-label > svg').click()
+  cy.get('.MuiDrawer-paper').should('not.have.css', 'visibility', 'hidden')
+  cy.findByText('SPAIN-MAP').click()
+  cy.get('.MuiDrawer-paper').should('have.css', 'visibility', 'hidden')
+
+});
+it('drawer closes on backdrop click, mobile', ()=> {
+  cy.viewport(500, 584)
+  cy.visit('/spain-map')
+    // burger icon
+  cy.get('#root > div > header > div > button > span.MuiIconButton-label > svg').click()
+  // drawer paper showed
+  cy.get('.MuiDrawer-paper').should('not.have.css', 'visibility', 'hidden')
+  // backdrop
+  cy.get('.MuiBackdrop-root').click()
+  // drawer paper hidden
+  cy.get('.MuiDrawer-paper').should('have.css', 'visibility', 'hidden')
 
 });
