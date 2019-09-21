b7843cd76a8fadc48778e7d7307797f15d2d1695
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-09-21 12:42:07 +0200

    Add e2e test for navigating to details page

---
+++ cypress/e2e/resultscontentitem.navigate_to_details.spec.js
@@ -0,0 +1,9 @@
+it('navigate to details page from when clicking beach name', ()=>{
+  cy.visit('/spain-map')
+  // select Cantabria
+  cy.get('#__AmCharts_React_1__ > div > div.amcharts-chart-div > svg > g:nth-child(8) > g > g:nth-child(1) > path:nth-child(5)').click()
+  const beachName = 'Oyambre'
+  const regex = new RegExp('' + beachName + '', 'i');
+  cy.findByText(regex, {timeout: 70000}).click()
+  cy.url().should('include', '/details/beach')
+});
