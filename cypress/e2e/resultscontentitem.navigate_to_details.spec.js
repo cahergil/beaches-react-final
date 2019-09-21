84b7eaa67ab7e44612c9877a4a5705958f39e607
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-09-21 13:04:47 +0200

    Replicate rtl integration test in cypress when navigating to details

--- cypress/e2e/resultscontentitem.navigate_to_details.spec.js
+++ cypress/e2e/resultscontentitem.navigate_to_details.spec.js
@@ -6,4 +6,28 @@ it('navigate to details page from when clicking beach name', ()=>{
   const regex = new RegExp('' + beachName + '', 'i');
   cy.findByText(regex, {timeout: 7000}).click()
   cy.url().should('include', '/details/beach')
+  cy.findAllByText(beachName).then(beachNameElements => {
+  const oyambreHeader = beachNameElements[0];
+  expect(oyambreHeader).to.have.text(beachName);
+ })
+});
+
+it('navigate to details page from when clicking beach image', ()=>{
+  cy.visit('/spain-map')
+  // select Cantabria
+  cy.get('#__AmCharts_React_1__ > div > div.amcharts-chart-div > svg > g:nth-child(8) > g > g:nth-child(1) > path:nth-child(5)').click()
+  const beachName = 'Oyambre'
+ 
+  cy.findAllByTestId('beach-image', {timeout: 7000}).then(images => {
+    const imageOyambre = images[0]
+    imageOyambre.click()
+    cy.url().should('contain', '/details/beach')
+    cy.findAllByText(beachName).then(beachNameElements => {
+      const oyambreHeader = beachNameElements[0];
+      expect(oyambreHeader).to.have.text(beachName);
+    
+    })
+  })
+
+  // cy.url().should('include', '/details/beach')
 });
