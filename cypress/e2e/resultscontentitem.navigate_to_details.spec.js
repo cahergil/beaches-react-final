c7f418a2d510615a80e93759f247b0f4727e578a
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-09-21 12:43:08 +0200

    Fix typo

--- cypress/e2e/resultscontentitem.navigate_to_details.spec.js
+++ cypress/e2e/resultscontentitem.navigate_to_details.spec.js
@@ -4,6 +4,6 @@ it('navigate to details page from when clicking beach name', ()=>{
   cy.get('#__AmCharts_React_1__ > div > div.amcharts-chart-div > svg > g:nth-child(8) > g > g:nth-child(1) > path:nth-child(5)').click()
   const beachName = 'Oyambre'
   const regex = new RegExp('' + beachName + '', 'i');
-  cy.findByText(regex, {timeout: 70000}).click()
+  cy.findByText(regex, {timeout: 7000}).click()
   cy.url().should('include', '/details/beach')
 });
